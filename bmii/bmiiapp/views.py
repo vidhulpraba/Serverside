0eef75286c75bce03147fe45768253c7cdba5d3b
vidhulpraba <vidhulpraba@gmail.com>
2025-12-15 11:10:26 +0530

    ho

---
+++ bmii/bmiiapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+def calculate_bmii(request):
+    bmii = None   # Default value
+
+    if request.method == "POST":
+        height = float(request.POST.get("height"))
+        weight = float(request.POST.get("weight"))
+        bmii = weight / (height * height)
+
+        # Print to server console for debugging
+        print("Height:", height)
+        print("Weight:", weight)
+        print("BMI calculated:", bmii)
+
+    return render(request, "bmiiapp/web.html", {"BMI": bmii})
+
+ 
